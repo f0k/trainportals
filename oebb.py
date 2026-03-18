58c32f0ad7b3ebfe8ab13a74656b70308daf50eb
Jan Schlüter <jan.schlueter@jku.at>
2026-03-18 09:17:11 +0100

    Support Westbahn-like trains run by OEBB

--- oebb.py
+++ oebb.py
@@ -9,15 +9,23 @@ import sys
 verbose = any(arg in sys.argv[1:] for arg in ('-v', '--verbose'))
 
 sess = requests.Session()
-portal_html = sess.get("http://detectportal.firefox.com").text
-if portal_html.startswith("success"):
+response = sess.head("http://detectportal.firefox.com")
+if response.status_code == 200 and response.headers['Content-Length'] == "8":
     if verbose:
         print("Already online.")
     sys.exit(0)
 
+portal_html = sess.get(response.headers["Location"]).text
 
-postdata = dict(re.findall(r'<input name="([^"]+)" type="hidden" value="([^"]+)">', portal_html))
-action_url = next(re.finditer(r'<form method="POST" action="([^"]+)"', portal_html))[1]
+if "oebb.at" in response.headers["Location"]:
+    postdata = dict(re.findall(r'<input name="([^"]+)" type="hidden" value="([^"]+)">', portal_html))
+    action_url = next(re.finditer(r'<form method="POST" action="([^"]+)"', portal_html))[1]
+elif "icomera" in response.headers["Location"]:
+    postdata = {}
+    action_url = "https://oebb.on.icomera.com/cna/logon"
+else:
+    print("Unknown portal:", response.headers["Location"])
+    sys.exit(1)
 if verbose:
     print("Logging in as",
           ", ".join(k + "=" + v for k, v in postdata.items()))
